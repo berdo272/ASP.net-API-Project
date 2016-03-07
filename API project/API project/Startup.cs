85e75c67a7a23236823a28d874163dff9e65fc0e
Robert Starrett <Robert Starrett>
2016-03-07 15:56:14 -0600

    initial commit

---
+++ API project/API project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(API_project.Startup))]
+namespace API_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
